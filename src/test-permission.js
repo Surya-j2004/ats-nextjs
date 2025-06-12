af7ef0bb26333db5cf34fa970dd4f0f66bb2dbb9
Surya-j2004 <159071057+Surya-j2004@users.noreply.github.com>
2025-06-12 17:04:25 +0530

    Add files via upload

---
+++ src/test-permission.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const uploadsDir = path.join(process.cwd(), 'uploads');
+
+try {
+  fs.accessSync(uploadsDir, fs.constants.W_OK);
+  console.log('uploads folder is writable');
+} catch (err) {
+  console.error('uploads folder is NOT writable:', err.message);
+}
