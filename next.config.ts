80c21614adf752e9a40b4c7281e916e16fd9c67d
Surya-j2004 <159071057+Surya-j2004@users.noreply.github.com>
2025-06-13 16:31:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-module.exports = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      "@aws-sdk/client-s3",
-      "@aws-sdk/s3-request-presigner"
-    ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during builds (fix issues locally)
+    ignoreDuringBuilds: true,
   },
+  serverExternalPackages: [
+    '@aws-sdk/client-s3',
+    '@aws-sdk/s3-request-presigner'
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@aws-sdk/client-s3': 'commonjs @aws-sdk/client-s3',
+        '@aws-sdk/s3-request-presigner': 'commonjs @aws-sdk/s3-request-presigner'
+      });
+    }
+    return config;
+  }
 };
+
+module.exports = nextConfig;
