bd5b8a326f0aed1c466c05a7101c0557b9f1b30a
Surya-j2004 <159071057+Surya-j2004@users.noreply.github.com>
2025-07-07 22:48:46 +0530

    Create authOptions.js

---
+++ src/lib/authOptions.js
@@ -0,0 +1,28 @@
+// src/lib/authOptions.js
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, token }) {
+      if (token) session.user.role = token.role;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
